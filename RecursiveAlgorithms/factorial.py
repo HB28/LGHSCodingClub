1422eeaf7b0941a115f9716c7759ff425ed6856b
Hunter <hunterb@chaosplan.com>
2021-02-04 10:00:25 -0800

    commit

---
+++ RecursiveAlgorithms/factorial.py
@@ -0,0 +1,9 @@
+def recursiveNum(start):
+    if(start == 0):
+        return 1
+    elif(start < 0):
+        return -1
+    return start * recursiveNum(start-1)
+
+val = int(input("Enter the value you would like to get the factorial of  "))
+print ("The factorial of " + str(val) + " is " + str(recursiveNum(val)))
